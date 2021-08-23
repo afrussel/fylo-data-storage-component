e712a48bf6b3c48cddc5fe40e694a715336040eb
afrussel <rasel365@gmail.com>
2021-08-24 00:47:24 +0600

    Added files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 300;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.min(Math.ceil(count + increment), target);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
